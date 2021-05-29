c9fcd8275bcfd21346dcd6562783169821dec9b0
LegendaryAy <ayobami.fadeni@decagon.dev>
2021-05-29 23:40:21 +0100

    test

--- GitMergeTest/Program.cs
+++ GitMergeTest/Program.cs
@@ -7,6 +7,7 @@ namespace GitMergeTest
         static void Main(string[] args)
         {
             Console.WriteLine("This is my first statement");
+            Console.WriteLine("This is my second statement but first from test branch");
         }
     }
 }
