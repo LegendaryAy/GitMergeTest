40ea091ff1d98f42a0bd14dc33c69293e74acb97
LegendaryAy <ayobami.fadeni@decagon.dev>
2021-05-29 23:37:54 +0100

    Add project files.

---
+++ GitMergeTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitMergeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first statement");
+        }
+    }
+}
